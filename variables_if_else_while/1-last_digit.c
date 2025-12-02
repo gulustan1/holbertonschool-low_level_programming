ba110ea7ee31209d97db1615245867e33e282954
gulustan1 <gulustanaydinn@gmail.com>
2025-12-02 10:57:27 +0000

    Fix Betty style: use tabs for indentation, remove trailing whitespace

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,18 +9,21 @@
  */
 int main(void)
 {
-        int n;
-        int lastd;
-	
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    lastd = n % 10;
-	    printf("Last digit of %d is %d ", n, lastd);
+	int n;
+	int lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+
+	printf("Last digit of %d is %d ", n, lastd);
+
 	if (lastd > 5)
 		printf("and is greater than 5\n");
 	else if (lastd == 0)
-                printf("and is 0\n");
+		printf("and is 0\n");
 	else
-                printf("and is less than 6 and not 0\n");
-        return (0);
+		printf("and is less than 6 and not 0\n");
+
+	return (0);
 }
