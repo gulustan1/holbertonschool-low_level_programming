81a736f202ec28636fe350493611cde51f1b5662
gulustan1 <gulustanaydinn@gmail.com>
2025-12-03 14:02:15 +0000

    .

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -15,22 +15,5 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-}#include <stdio.h>
-
-/**
- * main - Prints all single digit numbers of base 10
- *        starting from 0, followed by a new line
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int digit;
-
-	for (digit = 0; digit <= 9; digit++)
-		putchar('0' + digit);
-	putchar('\n');
-
-	return (0);
 }
 
