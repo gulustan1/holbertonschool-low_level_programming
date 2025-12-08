5f87ef7c472867bbfc33c7e30135622cd97e37dd
gulustan1 <gulustanaydinn@gmail.com>
2025-12-08 12:46:00 +0000

    .

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
-void reset_to_98(int *n);
-
-int main(void)
+/**
+ * reset_to_98 - updates the value of an int to 98
+ * @n: pointer to the int to be updated
+ */
+void reset_to_98(int *n)
 {
-    int a = 42;
-    printf("Before: %d\n", a);
-    reset_to_98(&a);
-    printf("After: %d\n", a);
-    return 0;
+    *n = 98;
 }
 
