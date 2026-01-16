6a92803a4c6ece07b476d8f4d79c926c26cd4228
gulustan1 <gulustanaydinn@gmail.com>
2026-01-16 19:22:55 +0000

    .

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stddef.h>
 
 /**
@@ -18,7 +17,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* Prototiplər */
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-#endif
+#endif /*LISTS_H*/
