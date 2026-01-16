d110dd3a3b4e443652378de7b7260a1c42c7662f
gulustan1 <gulustanaydinn@gmail.com>
2026-01-16 19:17:50 +0000

    .

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,14 +2,17 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the list_t list to print
+ * print_list - prints the all elements of the list
+ * @h: a pointer to the head of the list
  *
- * Return: the number of nodes in the list
+ * Return: number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t count;
+
+	count = 0;
 
 	while (h != NULL)
 	{
@@ -17,10 +20,8 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-		
-		h = h->next; /* Növbəti node-a keçid */
-		s++;
+		h = h->next;
+		count++;
 	}
-
-	return (s);
+	return (count);
 }
