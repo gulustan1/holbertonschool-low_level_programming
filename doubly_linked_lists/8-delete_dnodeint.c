859d8b562bd51a901c5939873378bba592f828b8
gulustan1 <gulustanaydinn@gmail.com>
2026-01-18 19:28:54 +0000

    .

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,45 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes a node
- * @head: a pointer to the head of the list
- * @index: an index
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
+ * @head: double pointer to the head of the list
+ * @index: index of the node to delete, starting at 0
  *
- * Return: int
+ * Return: 1 if it succeeded, -1 if it failed
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *current;
 	unsigned int i = 0;
-	dlistint_t *temp;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-	temp = *head;
+
+	current = *head;
+
+	/* Case 1: Deleting the head node (index 0) */
 	if (index == 0)
 	{
-		*head = temp->next;
-		if (temp->next != NULL)
-		{
-			temp->next->prev = NULL;
-		}
-		free(temp);
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
 		return (1);
 	}
-	while (temp != NULL && i < index)
+
+	/* Traverse to the node at the specified index */
+	while (current != NULL && i < index)
 	{
-		temp = temp->next;
+		current = current->next;
 		i++;
 	}
-	if (temp == NULL)
+
+	/* Case 2: Index is out of bounds */
+	if (current == NULL)
 		return (-1);
-	if (temp->prev != NULL)
-		temp->prev->next = temp->next;
-	if (temp->next != NULL)
-		temp->next->prev = temp->prev;
-	free(temp);
+
+	/* Case 3: Deleting a node in the middle or at the end */
+	/* Link the previous node to the next one */
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+
+	/* Link the next node back to the previous one (if not at the end) */
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+
+	free(current);
 	return (1);
 }
