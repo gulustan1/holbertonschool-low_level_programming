465685ef1f3e75f67dcab41a0895f44ccc6125d0
gulustan1 <gulustanaydinn@gmail.com>
2026-01-25 09:55:04 +0000

    .

---
+++ file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * create_file - creates a file and writes text_content to it
+ * @filename: name of the file
+ * @text_content: NULL-terminated string to write
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t wr;
+	size_t len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		len = strlen(text_content);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (len > 0)
+	{
+		wr = write(fd, text_content, len);
+		if (wr == -1 || (size_t)wr != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
